0f2a48c1c11bee97360aec4573da7397f6d1562d
Pierre <dev@net-tools.ovh>
2017-02-19 08:41:31 +0100

    exceptions handler

---
+++ src/ExceptionHandlers/ExceptionHandler.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Nettools\Core\ExceptionHandlers;
+
+
+use \Nettools\Core\Helpers\NetworkingHelper;
+
+
+
+/**
+ * Abstract class to format exception so that error lines could be displayed on screen and easily read
+ */
+abstract class ExceptionHandler
+{
+    /**
+     * Get a string with exception properly formatted
+     * 
+     * @param \Throwable $e Exception object
+     * @param string $h1 The title displayed on the error page
+     * @return string Returns a string with $e exception object properly formatted to be human-readable
+     */
+    abstract protected function _getException(\Throwable $e, $h1 = 'An error occured');	
+	
+    
+	
+	/**
+     * Get a link for mobile display (will be hidden if display is larger than 800px)
+     * 
+     * @param string $exstr Exception formatted as a string
+     * @return string Returns the string $exstr with an appended line to display a popup window
+     */
+	protected function _mobileLink($exstr)
+	{
+		// écriture affichage lien pour version mobile ; caché pour version normal et grand écran
+		$exattr = '<!DOCTYPE html><html><body>' . htmlspecialchars($exstr) . '</body></html>';
+		
+		$autohide = <<<AUTOHIDE
+
+/* version non mobile : masquer */
+@media screen and (min-width:800px) {
+	.mobile_link_exception {
+		display:none;
+		visibility:hidden;
+	}
+}
+
+AUTOHIDE;
+        
+        
+        $mobilelink = <<<LINK
+
+<style>
+.mobile_link_exception{
+	outline:none; 
+	background-color:antiquewhite; 
+    font-family: Gotham, Helvetica Neue, Helvetica, Arial," sans-serif";
+	color:black; 
+	font-size:12px; 
+	font-weight:bold; 
+	padding:3px; 
+	margin-top:10px; 
+	margin-bottom:10px; 
+	display:block; 
+	border-top:2px solid firebrick;
+	border-bottom:2px solid firebrick;
+}
+
+$autohide
+
+</style>
+
+<script>
+function openPage(a)
+{
+	var exw = window.open('', "ExceptionWindow" + (Math.floor(Math.random() * 10000000)));
+	if ( exw )
+	{
+		exw.document.write(a.getAttribute('data-exception'));
+		exw.document.close();
+	}
+	else
+		alert('Cannot open a popup window');
+}
+</script>
+
+
+
+<a href="javascript:void(0)" onclick="openPage(this); return false;" class="mobile_link_exception" data-exception="{$exattr}">Open a new page and display error data</a>
+
+LINK;
+		
+		return $mobilelink;
+	}
+    
+        
+    
+    /**
+     * Handle an exception during a GET/POST request and display in browser output a human-readable exception data
+     *
+     * The formatted exception is outputed directly to standard output.
+     *
+     * @param \Throwable $e Exception to format
+     */
+    protected function _handleGETPOSTException(\Throwable $e)
+    {
+        header("Content-Type: text/html; charset=utf-8");
+
+		// get exception string
+		$ex = $this->_getException($e, 'Error during request');
+		
+        echo '<!DOCTYPE html><html><body>';
+
+		// reset css and adapt font-size according to display width
+        echo <<<HTML
+<style>
+
+	body{
+		font-size:1em;		
+	}
+	
+	
+	/* width>1400 : larger font */
+	@media screen and (min-width:1400px) {
+		body {
+			font-size:1.1em;
+		}
+	}
+
+	
+	/* width < 800 : mobile screen, smaller font */
+	@media screen and (max-width:800px) {
+		body {
+			font-size:0.6em;
+		}
+	}
+</style>
+
+HTML;
+
+		echo $ex;
+		
+		// mobile link
+		echo $this->_mobileLink($ex);
+		echo '</body></html>';
+    }
+    
+    
+
+    /** 
+     * Handle an exception during an XMLHTTP request.
+     *
+     * The XMLHTTP request JSON response will contain a 'exception' property with the exception data formatted as a human-readable string.
+     * The JSON formatted exception is outputed directly to standard output.
+     *
+     * @param \Throwable $e Exception
+     */
+    protected function _handleXMLHTTPCommandException(\Throwable $e)
+    {
+		// send xmlhttp headers
+		header("Content-Type: application/json; charset=utf-8");
+		
+		// no cache
+		header("Expires: Sat, 1 Jan 2005 00:00:00 GMT");
+		header("Last-Modified: ".gmdate( "D, d M Y H:i:s")." GMT");
+		header("Cache-Control: no-cache, must-revalidate");
+		header("Pragma: no-cache"); 
+        
+        
+        // get exception as string
+        $ex = $this->_getException($e, 'Error during async request');
+        
+        echo json_encode(array('statut'=>false, 'message'=>'Error during async request', 'exception'=>$ex));
+    }
+    
+    
+	
+	/**
+     * Handle an exception
+     *
+     * @param \Throwable $e Exception to format
+     * @return string The exception formatted as a string
+     */
+	public function handleException(\Throwable $e)
+	{
+        // headers sent by browser are prefixed with 'HTTP' (so that they don't mess with environment variables), and - are replaced by _
+        if ( (strpos($_SERVER['HTTP_USER_AGENT'], 'XMLHTTP') === 0) || (strpos($_SERVER['HTTP_ACCEPT'], 'application/json') === 0) )
+            // if XMLHTTP request
+            $this->_handleXMLHTTPCommandException($e);
+
+        // if GET/POST request
+        else
+            $this->_handleGETPOSTException($e);
+        
+        die();
+    }   
+}
+
+?>
---
+++ src/ExceptionHandlers/SimpleExceptionHandler.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Nettools\Core\ExceptionHandlers;
+
+
+use \Nettools\Core\Helpers\NetworkingHelper;
+
+
+
+/**
+ * Abstract class to format exception so that error lines could be displayed on screen and easily read
+ */
+class SimpleExceptionHandler extends ExceptionHandler
+{
+    /**
+     * Get CSS code
+     *
+     * @return string CSS STYLE tag
+     */
+    protected function _outputCSS()
+    {
+        return <<<HTML
+<style>
+
+    div#bootstrap_exception_body{
+        font-family: Gotham, Helvetica Neue, Helvetica, Arial," sans-serif";
+    }
+
+    div#bootstrap_exception_body h1{
+        border-bottom: 2px solid firebrick;
+        padding-bottom: 5px;
+		font-size:2em;
+    }
+
+    div#bootstrap_exception_body h2{
+        color:#888;
+		font-size:1.5em;
+    }
+
+    div#bootstrap_exception_body code{
+        padding:5px;
+        background-color: antiquewhite;
+        font-size:0.8em;
+        font-family: Consolas, Andale Mono, Lucida Console, Lucida Sans Typewriter, Monaco, Courier New, monospace;
+    }
+
+    table#bootstrap_exception{
+        font-family: Consolas, Andale Mono, Lucida Console, Lucida Sans Typewriter, Monaco, Courier New, monospace;
+        font-size:0.7em;
+        border-spacing: 0;
+        margin-top:40px;
+    }
+
+    table#bootstrap_exception td,
+    table#bootstrap_exception th{
+        vertical-align: top;
+        padding:2px 5px;
+        white-space: nowrap;
+    }
+
+    table#bootstrap_exception td:nth-child(4){
+        white-space: pre;
+        display:none;
+    }
+
+    table#bootstrap_exception tr:nth-child(2n){
+        background-color: #ccc;
+    }
+</style>
+HTML;
+
+    }
+    
+    
+    /**
+     * Get a string with exception properly formatted
+     * 
+     * @param \Throwable $e Exception object
+     * @param string $h1 The title displayed on the error page
+     * @return string Returns a string with $e exception object properly formatted to be human-readable
+     */
+    protected function _getException(\Throwable $e, $h1 = 'An error occured')
+    {
+        // exception class
+        $kind = get_class($e);
+        
+        
+        $path_to_root = $_SERVER['DOCUMENT_ROOT'];
+        
+        
+        // output CSS
+        $ret = $this->_outputCSS();
+        
+        
+        // output JS / raw HTML
+        $ret .= <<<HTML
+<script>
+    function funParameters()
+    {
+        var tds = document.querySelectorAll('table#bootstrap_exception td:nth-child(4)');
+        var tdsl = tds.length;
+        for ( var i = 0 ; i < tdsl ; i++ )
+            tds[i].style.display = "block";
+    }
+</script>
+<div id="bootstrap_exception_body">
+<h1>$h1</h1>
+<h2>$kind</h2>
+<code>{$e->getMessage()}</code>
+
+<table id="bootstrap_exception">
+    <tr>
+        <th>File</th>
+        <th>Line</th>
+        <th>Function</th>
+        <th>Parameters <a href="javascript:void(0)" onclick="funParameters(); return false;">Display parameters</a></th>
+    </tr>
+HTML;
+
+
+        // handle stack trace
+        $ret .= "<tr><td>" . str_replace($path_to_root . '/', '', $e->getFile()) . "</td><td>" . $e->getLine() . "</td><td>throw new " . get_class($e) . " </td><td></td></tr>\n";
+        foreach ( $e->getTrace() as $trace )
+        {
+            $file = str_replace($path_to_root . '/', '', $trace['file']);
+            $line = $trace['line'];
+
+            if ( $trace['class'] )
+                $function = $trace['class'] . '::' . $trace['function'];
+            else
+                $function = $trace['function'];
+
+            $args = htmlspecialchars(print_r($trace['args'], true));
+
+            $ret .= "<tr><td>$file</td><td>$line</td><td>$function</td><td>$args</td></tr>\n";
+        }
+
+        $ret .= "</table></div>";
+        return $ret;   
+    }
+
+	
+}
+
+?>
