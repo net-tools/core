51446641cee22c0cf53d2b409c76cc8bd1a187e7
Pierre <dev@nettools.ovh>
2019-03-25 14:10:45 +0100

    bugfix codepage

--- tests/Helpers/EncodingHelperTest.php
+++ tests/Helpers/EncodingHelperTest.php
@@ -31,14 +31,14 @@ class EncodingHelperTest extends TestCase
 							'&Ugrave;&Uuml;&Ucirc;;' .
 							'&Ccedil;',
                             
-                            \Nettools\Core\Helpers\EncodingHelper::fr_entities_encode('â‚¬;Ã Ã¤Ã¢;Ã«ÃªÃ©Ã¨;Ã¯Ã®;Ã¶Ã´;Ã¹Ã¼Ã»;Ã§;Ã€Ã„Ã‚;Ã‹ÃŠÃ‰Ãˆ;Ã�ÃŽ;Ã–Ã”;Ã™ÃœÃ›;Ã‡')
+                            \Nettools\Core\Helpers\EncodingHelper::fr_entities_encode('€;àäâ;ëêéè;ïî;öô;ùüû;ç;ÀÄÂ;ËÊÉÈ;ÏÎ;ÖÔ;ÙÜÛ;Ç')
                         );
     }
     
     
     public function testFr_entities_decode()
     {
-        $this->assertEquals('â‚¬;Ã Ã¤Ã¢;Ã«ÃªÃ©Ã¨;Ã¯Ã®;Ã¶Ã´;Ã¹Ã¼Ã»;Ã§;Ã€Ã„Ã‚;Ã‹ÃŠÃ‰Ãˆ;Ã�ÃŽ;Ã–Ã”;Ã™ÃœÃ›;Ã‡',
+        $this->assertEquals('€;àäâ;ëêéè;ïî;öô;ùüû;ç;ÀÄÂ;ËÊÉÈ;ÏÎ;ÖÔ;ÙÜÛ;Ç',
                             \Nettools\Core\Helpers\EncodingHelper::fr_entities_decode('&euro;;' .
                                             '&agrave;&auml;&acirc;;' .
                                             '&euml;&ecirc;&eacute;&egrave;;'.
@@ -59,7 +59,7 @@ class EncodingHelperTest extends TestCase
     public function testnoAccents()
     {    
     	$this->assertEquals('accents:aaa;eeee;ii;oo;uuu;c;AAA;EEEE;II;OO;UUU;C', 
-                            \Nettools\Core\Helpers\EncodingHelper::noAccents('accents:Ã Ã¤Ã¢;Ã«ÃªÃ©Ã¨;Ã¯Ã®;Ã¶Ã´;Ã¹Ã¼Ã»;Ã§;Ã€Ã„Ã‚;Ã‰ÃˆÃ‹ÃŠ;Ã�ÃŽ;Ã–Ã”;Ã™ÃœÃ›;Ã‡')
+                            \Nettools\Core\Helpers\EncodingHelper::noAccents('accents:àäâ;ëêéè;ïî;öô;ùüû;ç;ÀÄÂ;ÉÈËÊ;ÏÎ;ÖÔ;ÙÜÛ;Ç')
                         );
     }
 }
