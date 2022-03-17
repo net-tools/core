f7d9a5657282e26653bf89a8a8e06e85001c5de9
Pierre <dev@nettools.ovh>
2022-03-17 10:13:04 +0100

    tests

--- tests/Helpers/DataHelperTest.php
+++ tests/Helpers/DataHelperTest.php
@@ -11,20 +11,7 @@ class DataHelperTest extends TestCase
     private $_locale = NULL;
 
     
-    public function setUp() :void
-    {
-        // backup current locale
-        $this->_locale = setlocale(LC_TIME, 0);
-    }
-    
-    
-    public function tearDown() :void
-    {
-        // restore locale
-        setlocale(LC_TIME, $this->_locale);
-    }
-    
-    
+   
     public function testNumber()
     {
         $this->assertSame('12,99', \Nettools\Core\Helpers\DataHelper::number(12.9889, 2));
@@ -75,8 +62,6 @@ class DataHelperTest extends TestCase
     
     public function testMonth2str()
     {
-        // set locale to en_US 
-        setlocale(LC_TIME, 'en_US');
         $this->assertEquals('January', \Nettools\Core\Helpers\DataHelper::month2str(1));
         $this->assertEquals('February', \Nettools\Core\Helpers\DataHelper::month2str(2));
         $this->assertEquals('March', \Nettools\Core\Helpers\DataHelper::month2str(3));
@@ -90,26 +75,23 @@ class DataHelperTest extends TestCase
         $this->assertEquals('November', \Nettools\Core\Helpers\DataHelper::month2str(11));
         $this->assertEquals('December', \Nettools\Core\Helpers\DataHelper::month2str(12));
 
-        setlocale(LC_TIME, 'fr_FR.utf8');
-        $this->assertEquals('janvier', \Nettools\Core\Helpers\DataHelper::month2str(1));
-        $this->assertEquals('février', \Nettools\Core\Helpers\DataHelper::month2str(2));
-        $this->assertEquals('mars', \Nettools\Core\Helpers\DataHelper::month2str(3));
-        $this->assertEquals('avril', \Nettools\Core\Helpers\DataHelper::month2str(4));
-        $this->assertEquals('mai', \Nettools\Core\Helpers\DataHelper::month2str(5));
-        $this->assertEquals('juin', \Nettools\Core\Helpers\DataHelper::month2str(6));
-        $this->assertEquals('juillet', \Nettools\Core\Helpers\DataHelper::month2str(7));
-        $this->assertEquals('août', \Nettools\Core\Helpers\DataHelper::month2str(8));
-        $this->assertEquals('septembre', \Nettools\Core\Helpers\DataHelper::month2str(9));
-        $this->assertEquals('octobre', \Nettools\Core\Helpers\DataHelper::month2str(10));
-        $this->assertEquals('novembre', \Nettools\Core\Helpers\DataHelper::month2str(11));
-        $this->assertEquals('décembre', \Nettools\Core\Helpers\DataHelper::month2str(12));
+        $this->assertEquals('janvier', \Nettools\Core\Helpers\DataHelper::month2str(1, 'fr_FR'));
+        $this->assertEquals('février', \Nettools\Core\Helpers\DataHelper::month2str(2, 'fr_FR'));
+        $this->assertEquals('mars', \Nettools\Core\Helpers\DataHelper::month2str(3, 'fr_FR'));
+        $this->assertEquals('avril', \Nettools\Core\Helpers\DataHelper::month2str(4, 'fr_FR'));
+        $this->assertEquals('mai', \Nettools\Core\Helpers\DataHelper::month2str(5, 'fr_FR'));
+        $this->assertEquals('juin', \Nettools\Core\Helpers\DataHelper::month2str(6, 'fr_FR'));
+        $this->assertEquals('juillet', \Nettools\Core\Helpers\DataHelper::month2str(7, 'fr_FR'));
+        $this->assertEquals('août', \Nettools\Core\Helpers\DataHelper::month2str(8, 'fr_FR'));
+        $this->assertEquals('septembre', \Nettools\Core\Helpers\DataHelper::month2str(9, 'fr_FR'));
+        $this->assertEquals('octobre', \Nettools\Core\Helpers\DataHelper::month2str(10, 'fr_FR'));
+        $this->assertEquals('novembre', \Nettools\Core\Helpers\DataHelper::month2str(11, 'fr_FR'));
+        $this->assertEquals('décembre', \Nettools\Core\Helpers\DataHelper::month2str(12, 'fr_FR'));
     }
     
     
     public function testMonth2shortstr()
     {
-        // set locale to en_US 
-        setlocale(LC_TIME, 'en_US');
         $this->assertEquals('Jan', \Nettools\Core\Helpers\DataHelper::month2shortstr(1));
         $this->assertEquals('Feb', \Nettools\Core\Helpers\DataHelper::month2shortstr(2));
         $this->assertEquals('Mar', \Nettools\Core\Helpers\DataHelper::month2shortstr(3));
@@ -124,19 +106,18 @@ class DataHelperTest extends TestCase
         $this->assertEquals('Dec', \Nettools\Core\Helpers\DataHelper::month2shortstr(12));
 
 
-        setlocale(LC_TIME, 'fr_FR.utf8');
-        $this->assertEquals('janv.', \Nettools\Core\Helpers\DataHelper::month2shortstr(1));
-        $this->assertEquals('févr.', \Nettools\Core\Helpers\DataHelper::month2shortstr(2));
-        $this->assertEquals('mars', \Nettools\Core\Helpers\DataHelper::month2shortstr(3));
-        $this->assertEquals('avril', \Nettools\Core\Helpers\DataHelper::month2shortstr(4));
-        $this->assertEquals('mai', \Nettools\Core\Helpers\DataHelper::month2shortstr(5));
-        $this->assertEquals('juin', \Nettools\Core\Helpers\DataHelper::month2shortstr(6));
-        $this->assertEquals('juil.', \Nettools\Core\Helpers\DataHelper::month2shortstr(7));
-        $this->assertEquals('août', \Nettools\Core\Helpers\DataHelper::month2shortstr(8));
-        $this->assertEquals('sept.', \Nettools\Core\Helpers\DataHelper::month2shortstr(9));
-        $this->assertEquals('oct.', \Nettools\Core\Helpers\DataHelper::month2shortstr(10));
-        $this->assertEquals('nov.', \Nettools\Core\Helpers\DataHelper::month2shortstr(11));
-        $this->assertEquals('déc.', \Nettools\Core\Helpers\DataHelper::month2shortstr(12));
+        $this->assertEquals('janv.', \Nettools\Core\Helpers\DataHelper::month2shortstr(1, 'fr_FR'));
+        $this->assertEquals('févr.', \Nettools\Core\Helpers\DataHelper::month2shortstr(2, 'fr_FR'));
+        $this->assertEquals('mars', \Nettools\Core\Helpers\DataHelper::month2shortstr(3, 'fr_FR'));
+        $this->assertEquals('avril', \Nettools\Core\Helpers\DataHelper::month2shortstr(4, 'fr_FR'));
+        $this->assertEquals('mai', \Nettools\Core\Helpers\DataHelper::month2shortstr(5, 'fr_FR'));
+        $this->assertEquals('juin', \Nettools\Core\Helpers\DataHelper::month2shortstr(6, 'fr_FR'));
+        $this->assertEquals('juil.', \Nettools\Core\Helpers\DataHelper::month2shortstr(7, 'fr_FR'));
+        $this->assertEquals('août', \Nettools\Core\Helpers\DataHelper::month2shortstr(8, 'fr_FR'));
+        $this->assertEquals('sept.', \Nettools\Core\Helpers\DataHelper::month2shortstr(9, 'fr_FR'));
+        $this->assertEquals('oct.', \Nettools\Core\Helpers\DataHelper::month2shortstr(10, 'fr_FR'));
+        $this->assertEquals('nov.', \Nettools\Core\Helpers\DataHelper::month2shortstr(11, 'fr_FR'));
+        $this->assertEquals('déc.', \Nettools\Core\Helpers\DataHelper::month2shortstr(12, 'fr_FR'));
     }
     
     
